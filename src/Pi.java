6949fea0a8a9c85cc4254c8b43e5a8a411a6e41f
devian131 <shalhar.namyrkozhoev@gmail.com>
2023-08-27 23:10:09 -0400

    Initial commit

---
+++ src/Pi.java
@@ -0,0 +1,10 @@
+public class Pi {
+    public static void main(String[] args) {
+        public int[] makePi() {
+            int [] piArray={3,1,4};
+            return piArray;
+
+        }
+
+    }
+}
